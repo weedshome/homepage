45d4778bf77c92d4b9407544a7843806e916d04b
dev <dev@Patricks-MacBook-Pro.local>
2020-09-14 16:25:58 +0300

    Added my project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import logo from '../assets/logo.png'
-import Links from '../constants/links'
 import SocialLinks from '../constants/socialLinks'
+import Hamburger from './Hamburger'
 
 const Navbar = ({ }) => {
   return <nav className="navbar">
@@ -12,8 +12,8 @@ const Navbar = ({ }) => {
           <img src={logo} alt="mdx logo" />
         </Link>
       </div>
-      <Links styleClass="nav-links" />
       <SocialLinks styleClass="nav-icons"></SocialLinks>
+      <Hamburger pageWrapId={"page-wrap"} outerContainerId={"App"} />
     </div>
   </nav>
 }
