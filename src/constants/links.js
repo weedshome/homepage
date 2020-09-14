08d9d9a678ae8cf8b02978d5b546e6876cd5752a
dev <dev@Patricks-MacBook-Pro.local>
2020-09-14 16:48:34 +0300

    Added my project

--- src/constants/links.js
+++ src/constants/links.js
@@ -6,58 +6,38 @@ import { TiArrowSortedDown } from 'react-icons/ti'
 const Links = ({ styleClass }) => {
   return (
     <ul className={styleClass}>
-      <li>
-        <Link to="/" className="page-link">
-          Home
+      <Link to="/" className="page-link">
+        Home
         </Link>
-      </li>
-      <li>
-        <Link to="/strains" className="page-link">
-          Strains
+      <Link to="/strains" className="page-link">
+        Strains
         </Link>
-      </li>
-      <li>
-        <div class="dropdown">
-          <button class="dropbtn" className="page-link">Blog<TiArrowSortedDown /></button>
-          <div class="dropdown-content">
-            <li>
-              <Link to="/news" className="page-link">
-                News
+      <div class="dropdown">
+        <button class="dropbtn" className="page-link">Blog<TiArrowSortedDown /></button>
+        <div class="dropdown-content">
+          <Link to="/news" className="page-link">
+            News
         </Link>
-            </li>
-            <li>
-              <Link to="/lifestyle" className="page-link">
-                Lifestyle
+          <Link to="/lifestyle" className="page-link">
+            Lifestyle
               </Link>
-            </li>
-            <li>
-              <Link to="/health" className="page-link">
-                Health
+          <Link to="/health" className="page-link">
+            Health
               </Link>
-            </li>
-            <li>
-              <Link to="/how-to" className="page-link">
-                How-To
+          <Link to="/how-to" className="page-link">
+            How-To
         </Link>
-            </li>
-            <li>
-              <Link to="/growing" className="page-link">
-                Growing
+          <Link to="/growing" className="page-link">
+            Growing
               </Link>
-            </li>
-          </div>
         </div>
-      </li>
-      <li>
-        <Link to="/business" className="page-link">
-          Business
+      </div>
+      <Link to="/business" className="page-link">
+        Business
         </Link>
-      </li>
-      <li>
-        <Link to="/contact" className="page-link">
-          Contact
+      <Link to="/contact" className="page-link">
+        Contact
         </Link>
-      </li>
     </ul>
   )
 }
