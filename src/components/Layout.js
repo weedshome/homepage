9689a62ad0d384543a56db8c5f77120513ed9d6f
dev <dev@Patricks-MacBook-Pro.local>
2020-09-14 14:38:19 +0300

    Added my project

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,22 +13,9 @@ import BeforeFooter from './BeforeFooter'
 import CookieConsent from 'react-cookie-consent';
 
 const Layout = ({ children }) => {
-  const [isOpen, setIsOpen] = React.useState(false)
-  const toggle = () => {
-    setIsOpen(!isOpen)
-  }
   return (
     <>
-      <Navbar />
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <BeforeFooter />
-      <CookieConsent
-        location="bottom"
-        buttonText="Accept"
-        style={{ background: "#102a42" }}
-        buttonStyle={{ background: "#6e9654", color: "13px", color: "#ffffff" }}
-        cookieName="gatsby-gdpr-google-analytics">
-        We use cookies to enable essential features of our site. Learn more in our <a href="/cookie-policy">Cookie Policy</a> and <a href="/cookie-policy">Privacy Policy.</a>    </CookieConsent>
+      <main>{children}</main>
       <Footer />
     </>
   )
