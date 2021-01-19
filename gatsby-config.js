b48cebb8dfdd3776089e36594acda2f4b0753953
dev <dev@Patricks-MacBook-Pro.local>
2021-01-19 15:11:43 +0200

    Added my project

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         forceFullSync: true,
         downloadLocal: true,
       },
