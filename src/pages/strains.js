8b9973d636cefe5058e962a0a61e31ce548436bd
weedshome <58329092+weedshome@users.noreply.github.com>
2021-02-09 08:44:12 +0200

    Update strains.js

--- src/pages/strains.js
+++ src/pages/strains.js
@@ -24,6 +24,7 @@ const Strains = ({ data }) => {
     <Wrapper>
       <Layout>
         <SEO title="Strains" schemaMarkup={schema} />
+        <div data-mantis-zone="captain-jack"></div>
         <div className="strains-background">
           <section className="posts">
             <article>
